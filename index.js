440d9f14df5ef36319416af779ed1be705722ac4
three-cubed <72698356+three-cubed@users.noreply.github.com>
2021-09-19 09:59:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname)); // To access stylesheet.css
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/other', (req, res) => {
+    res.render('other');
+})
+
+app.listen(process.env.port||3333, () => {
+    console.log('listening to 3333');
+});
